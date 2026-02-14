93bc04adcba2dbf9ab456f7283a27c15294b0c78
ArmenPolymathHovhannisyan <mastermindarmen@gmail.com>
2026-02-14 23:41:22 +0100

    Creeper explosion tested and destruction disabled successfully. Enderman code blocks added.

--- src/main/java/nogrief/ExplosionListener.java
+++ src/main/java/nogrief/ExplosionListener.java
@@ -4,6 +4,8 @@ import org.bukkit.entity.Creeper;
 import org.bukkit.event.EventHandler;
 import org.bukkit.event.Listener;
 import org.bukkit.event.entity.EntityExplodeEvent;
+import org.bukkit.entity.Enderman;
+import org.bukkit.event.entity.EntityChangeBlockEvent;
 
 public final class ExplosionListener implements Listener {
 
@@ -11,6 +13,13 @@ public final class ExplosionListener implements Listener {
     public void onEntityExplode(EntityExplodeEvent event) {
         if (event.getEntity() instanceof Creeper) {
             event.blockList().clear(); // no block destruction, explosion still happens
+        }
+    }
+
+    @EventHandler
+    public void onEndermanChangeBlock(EntityChangeBlockEvent event) {
+        if (event.getEntity() instanceof Enderman) {
+            event.setCancelled(true);
         }
     }
 }
