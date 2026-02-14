c7cb64cff2620c8cbdb14aaaba95f045ca8289c1
ArmenPolymathHovhannisyan <mastermindarmen@gmail.com>
2026-02-14 22:58:27 +0100

    Initial commit of no grief spigot plugin for Minecraft 1.21.11. No testing done yet.

---
+++ src/main/java/NoGrief/ExplosionListener.java
@@ -0,0 +1,16 @@
+package NoGrief;
+
+import org.bukkit.entity.Creeper;
+import org.bukkit.event.EventHandler;
+import org.bukkit.event.Listener;
+import org.bukkit.event.entity.EntityExplodeEvent;
+
+public final class ExplosionListener implements Listener {
+
+    @EventHandler
+    public void onEntityExplode(EntityExplodeEvent event) {
+        if (event.getEntity() instanceof Creeper) {
+            event.blockList().clear(); // no block destruction, explosion still happens
+        }
+    }
+}
---
+++ src/main/java/NoGrief/NoGrief.java
@@ -0,0 +1,18 @@
+package NoGrief;
+
+import org.bukkit.plugin.java.JavaPlugin;
+
+public final class NoGrief extends JavaPlugin {
+
+    @Override
+    public void onEnable() {
+        getServer().getPluginManager().registerEvents(new ExplosionListener(), this);
+        getLogger().info("NoCreeperGrief enabled.");
+
+    }
+
+    @Override
+    public void onDisable() {
+        // Plugin shutdown logic
+    }
+}
